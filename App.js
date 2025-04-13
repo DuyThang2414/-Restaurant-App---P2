1ad05c3af2a1a986db3d34d5ed5716f724ddad85
thang <thang2401204@gmail.com>
2025-04-13 14:31:21 +0700

    Update main files

--- App.js
+++ App.js
@@ -1,69 +1,24 @@
-import React, { useState } from "react";
-import { View, TextInput, Text, Button, StyleSheet } from "react-native";
+// App.js
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screen/Home";
+import Cart from "./screen/CartShop";
+import Profile from "./screen/Profile";
 
-const PhoneValidationApp = () => {
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [error, setError] = useState("");
+const Stack = createNativeStackNavigator();
 
-  const validatePhoneNumber = (number) => {
-    const phoneRegex = /^(\+84|0)(3|5|7|8|9)\d{8}$/;
-    return phoneRegex.test(number);
-  };
-
-  const handleSubmit = () => {
-    if (validatePhoneNumber(phoneNumber)) {
-      setError("");
-
-      alert("Số điện thoại hợp lệ");
-    } else {
-      setError("Số điện thoại không hợp lệ");
-    }
-  };
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Xác thực số điện thoại Việt Nam</Text>
-
-      <TextInput
-        style={styles.input}
-        placeholder="Nhập số điện thoại"
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
-        keyboardType="phone-pad"
-      />
-
-      {error ? <Text style={styles.errorText}>{error}</Text> : null}
-
-      <Button title="Xác thực" onPress={handleSubmit} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Cart" component={Cart} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 5,
-    fontSize: 16,
-  },
-  errorText: {
-    color: "red",
-    fontSize: 14,
-    marginBottom: 10,
-    textAlign: "center",
-  },
-});
-
-export default PhoneValidationApp;
+}
